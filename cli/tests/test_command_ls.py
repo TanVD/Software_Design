b366dabc6280d8b15ab25eb96c251e31bff25d0a
Vladislav Tankov <vdtankov@gmail.com>
2019-06-12 16:37:23 +0300

    Fix LS test

--- cli/tests/test_command_ls.py
+++ cli/tests/test_command_ls.py
@@ -5,9 +5,11 @@ from src.commands import CommandLs
 from src.storage import Storage
 
 
-class TestCommandCd(TestCase):
+class TestCommandLs(TestCase):
     def test_execute(self):
         storage = Storage(r'\$[^ \'\"$]+')
 
         command = CommandLs([os.path.dirname(os.path.dirname(__file__))])
-        self.assertEqual(command.execute("", storage), "class_diagram.png\nsrc\nREADME.md\ntests")
+
+        self.assertEqual(set(command.execute("", storage).split("\n")),
+                         {"class_diagram.png", "src", "README.md", "tests"})
